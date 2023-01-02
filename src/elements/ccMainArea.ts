4345b3118fc47ed79438f346968dc51ae8b0faac
ryoji <ryoji@unko.com>
2023-01-02 18:31:37 +0900

    字幕エリア用のクラス作成

--- src/elements/ccMainArea.ts
+++ src/elements/ccMainArea.ts
@@ -1,3 +1,18 @@
-export interface ccMainArea {
+import { selector } from "@/selector"
+
+export interface ccMainAreaInterface {
   hideCcArea(): void
+  getElement(): HTMLElement | null
+}
+
+export class CcMainArea implements ccMainAreaInterface {
+  hideCcArea(): void {
+    const element = this.getElement()
+    if (element === null) return
+    element.style.opacity = "0"
+  }
+
+  getElement(): HTMLElement | null {
+    return document.querySelector<HTMLElement>(selector.ccMainArea)
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import { ControlButton } from "./elements/controlButton"
-
+import { ControlButton } from "@/elements/controlButton"
+import { CcMainArea } from "@/elements/ccMainArea"
 const controlButton = new ControlButton()
 controlButton.createElement()
+
+const ccMainArea = new CcMainArea()
+ccMainArea.hideCcArea()
