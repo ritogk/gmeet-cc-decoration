67ebf38e469df42e2b096aadb822a2063ab0e8eb
ryoji <ryoji@unko.com>
2023-01-02 12:00:25 +0900

    作業武運

--- main.js
+++ main.js
@@ -1,43 +1,25 @@
-let beforeCC = ""
-// 変更を監視するノードを選択
-const targetNode = document.querySelector("#main")
-// (変更を監視する) オブザーバーのオプション
-// attributes: 属性の変更を関し
-// childList: 直接のこの
-// subtree: 監視対象のノードの子ノードも監視する
-// attributeFilter: 指定した属性のみを監視する
-// https://ja.javascript.info/mutation-observer
-const config = {
-  attributes: true,
-  subtree: true,
-  attributeFilter: ["span"],
-  // childList: true,
-  // characterData: true,
-  attributeOldValue: true,
-  // characterDataOldValue: true,
-}
-
-// 変更が発見されたときに実行されるコールバック関数
-const callback = function (mutationsList, observer) {
-  console.log("st")
-  for (const mutation of mutationsList) {
-    if (mutation.type === "childList") {
-      console.log("childList")
-      console.log(mutation)
-    } else if (mutation.type === "attributes") {
-      console.log("attributes")
-      console.log(mutation)
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
     }
-    if (mutation.type === "characterData") {
-      console.log("characterData")
-      console.log(mutation)
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports", "./test3"], factory);
     }
-  }
-  console.log("ed")
-}
-
-// コールバック関数に結びつけられたオブザーバーのインスタンスを生成
-const observer = new MutationObserver(callback)
-
-// 対象ノードの設定された変更の監視を開始
-observer.observe(targetNode, config)
+})(function (require, exports) {
+    "use strict";
+    var _a;
+    Object.defineProperty(exports, "__esModule", { value: true });
+    // import * as test from "./test"
+    // import * as test2 from "./test2"
+    const test3_1 = require("./test3");
+    function functet() {
+        // console.log(test.data.a)
+        // console.log(test.data.b)
+        // console.log(test2.data.c)
+        // console.log(test2.data.d)
+    }
+    const zip = new test3_1.ZipCodeValidator();
+    console.log(zip.isAcceptable("4"));
+    (_a = document.getElementById("main")) === null || _a === void 0 ? void 0 : _a.remove();
+});
