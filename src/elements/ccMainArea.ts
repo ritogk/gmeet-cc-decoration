ed8a5bc5e086c2bdd5b50f7d17cf46f76be4059e
ryoji <ryoji@unko.com>
2023-01-02 18:43:01 +0900

    クラスつくった

--- src/elements/ccMainArea.ts
+++ src/elements/ccMainArea.ts
@@ -2,14 +2,24 @@ import { selector } from "@/selector"
 
 export interface ccMainAreaInterface {
   hideElement(): void
+  showElement(): void
   getElement(): HTMLElement | null
 }
 
 export class CcMainArea implements ccMainAreaInterface {
+  hided = false
   hideElement(): void {
     const element = this.getElement()
     if (element === null) return
     element.style.opacity = "0"
+    this.hided = true
+  }
+
+  showElement(): void {
+    const element = this.getElement()
+    if (element === null) return
+    element.style.opacity = "1"
+    this.hided = false
   }
 
   getElement(): HTMLElement | null {
