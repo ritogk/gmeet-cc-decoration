03fe414cea8de4422a5fabf7d9889951f07d881f
ryoji <ryoji@unko.com>
2023-01-05 10:45:06 +0900

    入力値を検知できるようにした

--- chrome-extention/popup.js
+++ chrome-extention/popup.js
@@ -1,3 +1,38 @@
+function observe() {
+  const opacityRateElement = document.getElementsByName("opacityRate")[0]
+  opacityRateElement.addEventListener(
+    "change",
+    function (event) {
+      console.log(event.target.value)
+    },
+    false
+  )
+
+  const isDisplayOriginalCcElements = document.getElementsByName(
+    "isDisplayOriginalCc"
+  )
+  isDisplayOriginalCcElements[0].addEventListener(
+    "change",
+    function (event) {
+      if (!event.target.checked) return
+      console.log(event.target.value)
+    },
+    false
+  )
+  isDisplayOriginalCcElements[1].addEventListener(
+    "change",
+    function (event) {
+      if (!event.target.checked) return
+      console.log(event.target.value)
+    },
+    false
+  )
+}
+
+window.onload = (event) => {
+  observe()
+}
+
 // 現在アクティブなタブにデータを送信
 function sendToContents() {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
