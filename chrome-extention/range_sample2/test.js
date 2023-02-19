e8e569c34e0c3212e933a25211105b632a7a4caf
ryoji <ryoji@unko.com>
2023-02-19 15:34:04 +0900

    jqueryに依存しないようにした

--- chrome-extention/range_sample2/test.js
+++ chrome-extention/range_sample2/test.js
@@ -1,44 +1,40 @@
 window.onload = () => {
-  var $range = $(".range")
-  $range.each(function () {
-    var $thumb = $(this).next(".range-thumb")
-    var max = 10
+  var rangeDiv = document.getElementsByClassName("range-div")
+  for (let i = 0; i < rangeDiv.length; i++) {
+    const rangeDivElement = rangeDiv[i]
+    var thumbElement = rangeDivElement.getElementsByClassName("range-thumb")[0]
+    var rangeElement = rangeDivElement.getElementsByClassName("range")[0]
+    var max =
+      (Number(rangeElement.max) - Number(rangeElement.min)) /
+      Number(rangeElement.step)
     var tw = 17 // Thumb width. See CSS
 
-    $(this).on("input input.range", function () {
-      var w = $(this).width()
-      console.log(w)
-      var val = Math.floor(map(Number(this.value), 0.5, 1.5, 0, 10))
-      var txt = val >= max ? "1.5" : val
+    rangeElement.addEventListener("input", (event) => {
+      const value = event.target.value
+      var w = event.target.clientWidth
+      var val = Math.floor(map(Number(value), 0.5, 1.5, 0, 10))
       var xPX = (val * (w - tw)) / max
-      $thumb.css({ left: xPX }).attr("data-val", Number(this.value))
+      thumbElement.style.left = xPX
+      thumbElement.setAttribute("data-val", Number(value))
     })
-  })
-
-  $range.trigger("input.range") // Calc on load
-  $(window).on("resize", () => $range.trigger("input.range")) // and on resize
+    rangeElement.dispatchEvent(new Event("input"))
+  }
 }
 
 const map = (target_num, in_min, in_max, out_min, out_max) => {
   // 入力値(最大)と変換したい値の "差"
   const input_diff = in_max - target_num
-
   // 入力値の長さ
   const input_range = in_max - in_min
-
   // 出力値の長さ
   const output_range = out_max - out_min
-
   // (差 / 長さ) で "割合" を出す
   const percentage = input_diff / input_range
-
   // 前項の "割合" を使って "出力値側" の、最大値との "差" を出す
   const out_diff = percentage * output_range
-
   // "出力の最大値" から "出力値側の差" を除く
   // ※ "output_range" からではなく "out_max" から
   const rs = out_max - out_diff
-
   //結果
   return rs
 }
