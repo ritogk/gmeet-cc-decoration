f195742893c434bffbd8cac596ca42529096a87b
ryoji <ryoji@unko.com>
2023-02-19 15:58:03 +0900

    リファクタ

--- chrome-extention/range_sample2/test.js
+++ chrome-extention/range_sample2/test.js
@@ -1,21 +1,22 @@
 window.onload = () => {
-  var rangeDiv = document.getElementsByClassName("range-div")
+  const rangeDiv = document.getElementsByClassName("range-div")
   for (let i = 0; i < rangeDiv.length; i++) {
     const rangeDivElement = rangeDiv[i]
-    var thumbElement = rangeDivElement.getElementsByClassName("range-thumb")[0]
-    var rangeElement = rangeDivElement.getElementsByClassName("range")[0]
+    const thumbElement =
+      rangeDivElement.getElementsByClassName("range-thumb")[0]
+    const rangeElement = rangeDivElement.getElementsByClassName("range")[0]
     const max =
       (Number(rangeElement.max) - Number(rangeElement.min)) /
       Number(rangeElement.step)
-    var tw = thumbElement.clientWidth
+    const tw = thumbElement.clientWidth
 
     rangeElement.addEventListener("input", (event) => {
       const value = event.target.value
-      var w = event.target.clientWidth
-      var val =
+      const w = event.target.clientWidth
+      const val =
         (Number(rangeElement.value) - Number(rangeElement.min)) /
         Number(rangeElement.step)
-      var xPX = (val * (w - tw)) / max
+      const xPX = (val * (w - tw)) / max
       thumbElement.style.left = xPX
       thumbElement.setAttribute("data-val", Number(value))
     })
