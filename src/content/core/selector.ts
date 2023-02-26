706ee46dc1e75ec44119ce0aceffebfc363814ee
ryoji <ryoji@unko.com>
2023-02-26 09:26:52 +0900

    セレクターの範囲をゆるくした

--- src/content/core/selector.ts
+++ src/content/core/selector.ts
@@ -1,15 +1,16 @@
 /**
  * GoogleMeetのElementのセレクター
+ * セレクターの「div:nth-child(13)」の要素番号が14だったり13だったりでしょっちゅう変化する。
+ * 「#ow3 > div.T4LgNb > div > div:nth-child(13) > div.crqnQb」まではどのElementも同じ
+ *  #ow3 > div.T4LgNb > div > div:nth-child(13) > div.crqnQb > div.J0M6X.nulMpf.Didmac.G03iKb > div > div > div.Tmb7Fd > div > div.juFBl
  */
 const controlCcButton =
-  "#ow3 > div.T4LgNb > div > div:nth-child(13) > div.crqnQb > div.J0M6X.nulMpf.Didmac.G03iKb > div > div > div.Tmb7Fd > div > div.juFBl"
+  "div.crqnQb > div.J0M6X.nulMpf.Didmac.G03iKb > div > div > div.Tmb7Fd > div > div.juFBl"
 const ccMainArea = ".a4cQT"
-const ccArea =
-  "#ow3 > div.T4LgNb > div > div:nth-child(13) > div.crqnQb > div.a4cQT > div:nth-child(1) > div:nth-child(1)"
-const usersArea =
-  "#ow3 > div.T4LgNb > div > div:nth-child(13) > div.crqnQb > div:nth-child(2) > div.axUSnc.P9KVBf"
+const ccArea = "div.crqnQb > div.a4cQT > div:nth-child(1) > div:nth-child(1)"
+const usersArea = "div.crqnQb > div:nth-child(2) > div.axUSnc.P9KVBf"
 const controlArea =
-  "#ow3 > div.T4LgNb > div > div:nth-child(13) > div.crqnQb > div.J0M6X.nulMpf.Didmac.G03iKb > div > div > div.Tmb7Fd > div"
+  "div.crqnQb > div.J0M6X.nulMpf.Didmac.G03iKb > div > div > div.Tmb7Fd > div"
 
 const selector = {
   controlArea: controlArea,
