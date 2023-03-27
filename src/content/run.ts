0e7c0dd72b2654aa322c64a25570f07086d769cd
ryoji <ryoji@unko.com>
2023-03-27 10:37:23 +0900

    slack送信機能実装

--- src/content/run.ts
+++ src/content/run.ts
@@ -4,6 +4,7 @@ import { ControlCcButtonElement } from "@/content/elements/original/controlCcBut
 import { UsersAreaElement } from "@/content/elements/original/UsersAreaElement"
 import { main } from "@/content/main"
 import { Selector } from "@/content/core/selector"
+import { sendMessage } from "@/core/slack"
 
 const run = async () => {
   const selector = Selector.getInstance()
@@ -13,16 +14,34 @@ const run = async () => {
   const controlAreaElement = new ControlAreaElement()
   const controlCcButtonElement = new ControlCcButtonElement()
   const usersAreaElement = new UsersAreaElement()
+
+  // 会議画面が表示された時間
+  let meetShowedTime = 0
   const jsLoaded = (): void => {
+    // URLが会議画面(https://meet.google.com/xxx-yyy-zzzz)でない場合は処理させない
+    if (location.href === "https://meet.google.com/") return
+    if (meetShowedTime === 0) {
+      meetShowedTime = Date.now()
+    }
     // 全ての要素が描画されるまで待つ
-    if (
-      ccAreaElement.getElement() &&
-      controlAreaElement.getElement() &&
-      controlCcButtonElement.getElement() &&
-      usersAreaElement.getElement()
-    ) {
+    const ccArea = ccAreaElement.getElement()
+    const controlArea = controlAreaElement.getElement()
+    const controlCcButton = controlCcButtonElement.getElement()
+    const usersArea = usersAreaElement.getElement()
+    if (ccArea && controlArea && controlCcButton && usersArea) {
       clearInterval(jsInitCheckTimer)
       main()
+    } else {
+      if (Date.now() - meetShowedTime >= 15000) {
+        const errorReport = [
+          { ccArea: ccArea ? "○" : "✕" },
+          { controlArea: controlArea ? "○" : "✕" },
+          { controlCcButton: controlCcButton ? "○" : "✕" },
+          { usersArea: usersArea ? "○" : "✕" },
+        ]
+        sendMessage(JSON.stringify(errorReport))
+        clearInterval(jsInitCheckTimer)
+      }
     }
   }
   const jsInitCheckTimer = setInterval(jsLoaded, 1000)
