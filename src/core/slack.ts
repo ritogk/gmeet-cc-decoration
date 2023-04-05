621e6f4dcfbb2e16f5053a885a94226591f14210
ryoji <ryoji@unko.com>
2023-04-05 09:13:05 +0900

    apiclinetを使う形に変えた

--- src/core/slack.ts
+++ src/core/slack.ts
@@ -1,15 +1,11 @@
+import { SlackApi, Channel } from "@ritogk/private-api"
+const slackApi = new SlackApi()
+
 export const sendMessage = (text: string) => {
-  const params = {
-    channel: "#general",
-    username: "エラーレポート(GoogleMeetリラックス)",
-    text: text,
-    iconEmoji: ":ghost:",
-  }
-  const query = new URLSearchParams(params)
-  fetch(
-    `https://script.google.com/macros/s/AKfycbwwJBYjGJQAvTN1DJN8hlKQn6haUugmz6RHHpmX4slGbJfcLhwf3HhP__pwHJXNxyIr/exec?${query}`,
-    { method: "POST" }
-  )
-    .then((response) => console.log(response))
-    .catch((error) => console.error(error))
+  slackApi.apiSlackSendPost({
+    apiSlackSendPostRequest: {
+      channel: Channel.GENERAL,
+      text: `GoogleMeetリラックス[エラーレポート]\n${text}`,
+    },
+  })
 }
